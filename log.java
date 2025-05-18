f61f228b17cb6e9b46a8436d1ed745b202046dbd
Koda <sbevesays@gmail.com>
2025-05-18 13:05:06 -0700

    init commit

---
+++ log.java
@@ -0,0 +1,7 @@
+public interface log {
+    public void info(String message);
+    public void warn(String message);
+    public void error(String message);
+    public void debug(String message);
+    public void fatal(String message);
+}
